35b68f04e191e3df919d5336d6cd1300c5c37f68
Sapnil <bhowmiksapnil66138@gmail.com>
2025-05-31 09:35:47 +0530

    schedule at 10am

--- src/utils/cronJob.js
+++ src/utils/cronJob.js
@@ -5,7 +5,7 @@ const { generateCSVData } = require('./generateCSV');
 
 
 
-cron.schedule('0 9 * * *', async() => {
+cron.schedule('0 10 * * *', async() => {
     const csvFilePath = await generateCSVData()
     await sendMail("sapnilfrontend23@gmail.com" , csvFilePath)
 });
